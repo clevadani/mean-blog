ff72cceea89c475ce106321e3a0f996e1b81d5f9
Dani Ad <clevadani@gmail.com>
2018-08-27 01:05:04 +0100

    Minor changes

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto').randomBytes(356).toString('hex');
 module.exports = {
     uri:'mongodb://meanblog:meanblog1@ds247290.mlab.com:47290/meanblog004',
+    // uri:'mongodb://127.0.0.1/mean-angular-2',
     secret: crypto,
     db: 'meanblog004'
 }
--- models/user.js
+++ models/user.js
@@ -4,19 +4,19 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt-nodejs');
 
 let emailLengthChecker = (email) => {
-    if (!email) {
-        return false;
-    }else {
-        if (email.length < 5 || email.length > 30) {
-            return false;
-        }else {
-            return true;
-        }
-    }
+	if (!email) {
+		return false;
+	} else {
+		if (email.length < 5 || email.length > 30) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
 
 let validEmailChecker = (email) => {
-	if(!email) {
+	if (!email) {
 		return false;
 	} else {
 		const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -37,22 +37,22 @@ const emailValidators = [
 
 let usernameLengthChecker = (username) => {
 	if (!username) {
+		return false;
+	} else {
+		if (username.length < 3 || username.length > 15) {
 			return false;
-	}else {
-			if (username.length < 3 || username.length > 15) {
-					return false;
-			}else {
-					return true;
-			}
+		} else {
+			return true;
+		}
 	}
 }
 
 let validUsernameChecker = (username) => {
-	if(!username) {
-			return false;
+	if (!username) {
+		return false;
 	} else {
-			const re = /^[a-zA-Z0-9]+$/;
-			return re.test(username);
+		const re = /^[a-zA-Z0-9]+$/;
+		return re.test(username);
 	}
 }
 
@@ -69,19 +69,19 @@ const usernameValidators = [
 
 let passwordLengthChecker = (password) => {
 	if (!password) {
-			return false;
-	}else {
+		return false;
+	} else {
 		if (password.length < 8 || password.length > 35) {
 			return false;
-		}else {
-				return true;
+		} else {
+			return true;
 		}
 	}
 }
 
 let validPasswordChecker = (password) => {
-	if(!password) {
-			return false;
+	if (!password) {
+		return false;
 	} else {
 		const re = /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/;
 		return re.test(password);
@@ -101,24 +101,24 @@ const passwordValidators = [
 
 
 const userSchema = new Schema({
-    email: {type: String, required: true, unique: true, lowercase: true, validate: emailValidators},
-    username: {type: String, required: true, unique: true, lowercase: true, validate: usernameValidators},
-    password: {type: String, required: true, validate: passwordValidators}
+	email: { type: String, required: true, unique: true, lowercase: true, validate: emailValidators },
+	username: { type: String, required: true, unique: true, lowercase: true, validate: usernameValidators },
+	password: { type: String, required: true, validate: passwordValidators }
 });
 
 userSchema.pre('save', function (next) {
-    if (!this.isModified('password'))
-    return next();
+	if (!this.isModified('password'))
+		return next();
 
-    bcrypt.hash(this.password, null, null, (err, hash) => {
-			if (err) return next(err);
-			this.password = hash;
-			next();
-		})
+	bcrypt.hash(this.password, null, null, (err, hash) => {
+		if (err) return next(err);
+		this.password = hash;
+		next();
+	})
 });
 
-userSchema.methods.comparePassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
+userSchema.methods.comparePassword = function (password) {
+	return bcrypt.compareSync(password, this.password);
 }
 
 
